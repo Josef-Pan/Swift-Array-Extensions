12832f436d7a0fac9c1fdfc143155d737e1fac28
Josef <panjunfeng@hotmail.com>
2022-04-11 16:55:58 +1000

    Update ArrayExtension.swift

--- ArrayExtension.swift
+++ ArrayExtension.swift
@@ -1,35 +1,73 @@
-import UIKit
+import Foundation
 
 extension Array where Element: Comparable, Element: Hashable {
+    
     /// Count unique elements of Array
+    /// - Returns: number of unique elements in array
     func countUniques() -> Int {
         return reduce(into: []) { result, element in
             result.contains(element) ? () : result.append(element) // Append if not in 'result'
         }.count
     }
+    
     /// Remove only adjacent duplicates from Array, keeping the first occurence of duplicated elements
+    /// - Returns: array without adjacent duplicates
     func removeAdjacentDuplicates() -> [Element] {
         return reduce(into: []) { result, element in
             result.last != element ? result.append(element) : () // Append if element not equal to last one
         }
     }
+    
+    /// Remove only adjacent duplicates from Array, keeping the first occurence of duplicated elements
+    /// Unlike previous func, this function operates on the original Array
+    mutating func removeAdjacentDuplicatesInplace(){
+        var indicesToRemove :[Int] = []
+        self.enumerated().forEach{ idx, value in
+            guard idx > 0 else { return } // Search from idx == 1
+            value == self[idx-1] ? indicesToRemove.append(idx) : ()
+        }
+        indicesToRemove.enumerated().forEach{ self.remove(at: $1 - $0 )} // value - index
+    }
+    
     /// Remove all duplicate elements from Array, keeping only the first occurence
+    /// - Returns: array without duplicates
     func removeAllDuplicates()-> [Element] {
         return reduce(into: []) { result, element in
             result.contains(element) ? (): result.append(element) // Append if not in 'result'
         }
     }
+    
+    /// Remove all duplicate elements from Array, keeping only the first occurence
+    /// Unlike previous func, this function operates on the original Array
+    mutating func removeAllDuplicatesInplace(){
+        var indicesToRemove :[Int] = []
+        self.enumerated().forEach{ idx, value in
+            guard idx > 0 else { return } // Search from idx == 1
+            self[0..<(idx-1)].contains(value) ? indicesToRemove.append(idx) : ()
+        }
+        indicesToRemove.enumerated().forEach{ self.remove(at: $1 - $0 )} // value - index
+    }
+    
     /// Remove all elements that are duplicating from Array, not keeping of any occurrences of repeating elements
+    /// - Returns: array with all non-unique elements removed
     func removeAllDuplicatingElements()-> [Element] {
         let counts = reduce(into: [:]){ result, element in
             result[element, default:0 ] += 1
         } // let counts = reduce(into: [:]) { $0[$1, default: 0] += 1 } // Not readable, not encouraged
-        return filter{ element in
-            guard let count = counts[element] else { return false}
-            return count>1 ? false : true    // Element with occurrences more than 1 are duplicating
-        }
+        return filter{counts[$0, default:0] > 1}
     }
+    
+    /// Remove all elements that are duplicating from Array, not keeping of any occurrences of repeating elements
+    /// Unlike previous func, this function operates on the original Array
+    mutating func removeAllDuplicatingElementsInplace(){
+        let counts = reduce(into: [:]){ result, element in
+            result[element, default:0 ] += 1
+        }
+        self.removeAll(where: {counts[$0, default:0] > 1})
+    }
+    
     /// Test if Array has duplicates
+    /// - Returns: true if array has duplicates
     func hasDuplicates() ->Bool {
         let counts = reduce(into: [:]){ result, element in
             result[element, default:0 ] += 1
@@ -37,7 +75,9 @@ extension Array where Element: Comparable, Element: Hashable {
         let dups =  counts.values.max() ?? 0    //Get the maximum counts of elements
         return dups > 1 ? true : false          //Duplicates should have count more than 1
     }
+    
     /// Get first non repeating element from Array, may be nil
+    /// - Returns: first non-repeating element, maybe nil
     func firstNonRepeatingElement() -> Element? {
         let counts = reduce(into: [:]){ result, element in
             result[element, default:0 ] += 1
@@ -45,7 +85,9 @@ extension Array where Element: Comparable, Element: Hashable {
         let uique_index = self.firstIndex(where: {counts[$0] == 1})
         return uique_index == nil ? nil : self[uique_index!]
     }
+    
     /// Test if same values in Array are adjacent or not, [ 1, 1, 2, 2 ] ->true, [ 1, 1, 2, 2, 3 ] ->true, [ 2, 1, 1, 2 ] -> false
+    /// - Returns: true if all grouped
     func isAllGrouped() ->Bool {
         let counts = removeAdjacentDuplicates().reduce(into: [:]){ result, element in
             result[element, default:0 ] += 1
@@ -56,9 +98,11 @@ extension Array where Element: Comparable, Element: Hashable {
         }
         return true
     }
+    
     /// Get all combinations from Array, apply filter { $0.count ==n } to get combos of n elements
-    /// This function does not remove duplicates by default
+    /// ‼️This function does not remove duplicates by default
     /// Use removeAllDuplicates().getAllCombinations() to have cominations for uique elements
+    /// - Returns: all combinations of the original array, note that the empty array is also included
     func getAllCombinations() ->[[Element]] {
         guard count > 0 else { return [[]] }
         let head = self[0]                      // head contains only the first element
@@ -67,9 +111,11 @@ extension Array where Element: Comparable, Element: Hashable {
         let withHead = withoutHead.map { $0 + [head] }  // merging the head with the tail's powerset
         return withHead + withoutHead   // returning the tail's powerset and the just computed withHead array
     }
+    
     /// Get all permuations of an array
-    /// This function does not remove duplicates by default
+    /// ‼️This function does not remove duplicates by default
     /// Use removeAllDuplicates().getPermutations() to have cominations for uique elements
+    /// - Returns: all permutations of the original array, note that the empty array is also included
         func getPermutations() ->[[Element]]  {
         guard self.count > 1 else { return [self] } // Permutation of single element array is the array itself
         var permuations:[[Element]] = []
@@ -82,7 +128,9 @@ extension Array where Element: Comparable, Element: Hashable {
         }
         return permuations
     }
+    
     /// Test if contains subarray
+    /// Not fully optimised, for large array, do not use this, there are better solutions
     func containsSubarray(subArray:[Element])->Bool{
         guard subArray.count <= self.count else { return false }
         for i in 0...self.count-subArray.count{
@@ -91,18 +139,22 @@ extension Array where Element: Comparable, Element: Hashable {
         }
         return false
     }
+    
     /// Use 1D array with * to generate a 2D array
     /// [1, 3, 5] * 2 =[ [1, 3, 5], [1, 3, 5]]
     /// Not fully optimized, may have better solutions
+    /// - Returns: a 2D array with  rhs rows of the the original 1D array
     static func * (lhs: [Element], rhs: Int) ->[[Element]] {
         guard rhs > 0 else { return [] }
         var final:[[Element]] = []
         (0..<rhs).forEach{ $0; final.append(lhs) } // $0 has no operation, just required by forEach
         return final
     }
+    
     /// Use 2D array with * to generate a 3D array
     /// [ [1, 3, 5], [1, 3, 5] ] * 2 =[ [ [1, 3, 5], [1, 3, 5] ], [ [1, 3, 5], [1, 3, 5] ] ]
     /// Not fully optimized, may have better solutions
+    /// - Returns: a 3D array with  rhs layers of the the original 2D array
     static func * (lhs: [[Element]], rhs: Int) ->[[[Element]]] {
         guard rhs > 0 else { return [] }
         var final:[[[Element]]] = []
@@ -110,6 +162,7 @@ extension Array where Element: Comparable, Element: Hashable {
         return final
     }
 }
+
 extension Array  {  // let array contains() method to deal with tuple
     func contains<E1, E2>(_ tuple: (E1, E2)) -> Bool where E1: Equatable, E2: Equatable, Element == (E1, E2) {
         return contains { $0.0 == tuple.0 && $0.1 == tuple.1 }
